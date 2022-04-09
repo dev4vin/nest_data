e391f7cd7a059ab1d5d8737871cdb4b70f4117f7
Peter Vincent <39063789+dev4vin@users.noreply.github.com>
2022-04-09 10:35:06 +0300

    updates deps

--- src/entity.visitor.ts
+++ src/entity.visitor.ts
@@ -2,11 +2,6 @@ import { Visitor, DeepCopy } from "@dev4vin/commons";
 import { Injectable } from "@nestjs/common";
 import { BaseEntity } from "typeorm";
 
-
-
-
-
-
 @Injectable()
 export class EntityVisitor<T extends BaseEntity> implements Visitor<T, T> {
     visit(t: T, _: any = []): T {
